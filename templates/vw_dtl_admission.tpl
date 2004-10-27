99bd6a4d3fdd217ce92b35279eab5cd5b230217d
rhum1 <rhum1>
2004-10-27 09:54:38 +0000

    ajout des informations du patient dans les details d'une admission

--- templates/vw_dtl_admission.tpl
+++ templates/vw_dtl_admission.tpl
@@ -1,16 +1,85 @@
 <table class="main">
   <tr>
+  <td>
+  
+  
+      <table class="form">
+        <tr>
+          <th class="category" colspan="2">Identité</th>
+          <th class="category" colspan="2">Information médicales</th>
+        </tr>
+
+        <tr>
+          <th>Nom:</th>
+          <td>{$op.pat_lastname}</td>
+          <th>Incapable majeur:</th>
+          <td>
+            {if $op.incapable_majeur == "o"} oui {/if}
+            {if $op.incapable_majeur == "n"} non {/if}
+          </td>
+        </tr>
+        
+        <tr>
+          <th>Prénom:</th>
+          <td>{$op.pat_firstname}</td>
+          <th>ATNC:</th>
+          <td>
+            {if $op.ATNC == "o"} oui {/if}
+            {if $op.ATNC == "n"} non {/if}
+        </tr>
+        
+        <tr>
+          <th>Date de naissance:</th>
+          <td>{$op.naissance|date_format:"%d-%m-%Y"}</td>
+        </tr>
+        
+        <tr>
+          <th>Sexe:</th>
+          <td>
+            {if $op.sexe == "m"} masculin {/if}
+            {if $op.sexe == "f"} féminin  {/if} 
+          </td>
+        </tr>
+        
+        <tr>
+          <th class="category" colspan="2">Coordonnées</th>
+          <th class="category" colspan="2">Information administratives</th>
+        </tr>
+        
+        <tr>
+          <th>Adresse:</th>
+          <td>{$op.adresse}</td>
+          <th>Numéro d'assuré social:</th>
+          <td>{$op.matricule}</td>
+        </tr>
+        
+        <tr>
+          <th>Ville:</th>
+          <td>{$op.ville}</td>
+          <th>Code administratif:</th>
+          <td>{$op.SHS}</td>
+        </tr>
+        
+        <tr>
+          <th>Code Postal:</th>
+          <td>{$op.cp}</td>
+        </tr>
+        
+        <tr>
+          <th>Téléphone:</th>
+          <td>{$op.tel}</td>
+        </tr>
+
+      </table>
+  
+  </td>
     <td>
 	
       <table class="form">
         <tr><th class="category" colspan="2">Informations concernant l'opération</th></tr>
         <tr>
-		      <th class="mandatory">Chirurgien:</th>
+		      <th>Chirurgien:</th>
           <td>{$op.chir_name}</td>
-        </tr>
-        <tr>
-          <th class="mandatory">Patient:</th>
-          <td>{$op.pat_name}</td>
         </tr>
         <tr>
           <th>Diagnostic (CIM10):</th>
@@ -21,7 +90,7 @@
           <td>{$op.CCAM_code}</td>
         </tr>
         <tr>
-          <th class="mandatory">Temps opératoire:</th>
+          <th>Temps opératoire:</th>
           <td colspan="2">{$op.hour_op}:{$op.min_op}</td>
         </tr>
         <tr>
--- vw_dtl_admission.php
+++ vw_dtl_admission.php
@@ -1,4 +1,12 @@
-<?php
+<?php /* $Id$ */
+
+/**
+* @package Mediboard
+* @subpackage dPadmissions
+* @version $Revision$
+* @author Romain Ollivier
+*/
+
 GLOBAL $AppUI, $canRead, $canEdit, $m;
 
 if (!$canRead) {			// lock out users that do not have at least readPermission on this module
@@ -11,22 +19,30 @@ if(dPgetParam($_GET, "id", "noid") != "noid")
 }
 else
 {
-  $AppUI->msg = "Vous devez choisir une admission";
-  $AppUI->redirect( "m=dPadmissions&tab=0" );
+  if($_SESSION[$m][$tab]["id"] != "")
+    $id = $_SESSION[$m][$tab]["id"];
+  else {
+    $AppUI->msg = "Vous devez choisir une admission";
+    $AppUI->redirect( "m=dPadmissions&tab=0" );
+  }
 }
 
 $sql = "select operations.operation_id as id, operations.chir_id as chir_id,
 		users.user_first_name as chir_firstname, users.user_last_name as chir_lastname,
-		operations.pat_id as pat_id, patients.nom as pat_lastname, patients.prenom as pat_firstname,
-		operations.CIM10_code as CIM10_code, operations.CCAM_code as CCAM_code,
-		operations.temp_operation as temp_op,
+		operations.pat_id as pat_id, operations.CIM10_code as CIM10_code,
+        operations.CCAM_code as CCAM_code, operations.temp_operation as temp_op,
 		operations.plageop_id as plageop_id, plagesop.date as date_op,
 		operations.examen as examen, operations. materiel as materiel,
 		operations.info as info, operations.duree_hospi as duree_hospi,
 		operations.date_anesth as rdv_anesth, operations.time_anesth as time_anesth,
 		operations.date_adm as rdv_adm, operations.time_adm as time_adm,
 		operations.type_adm as type_adm, operations.chambre as chambre, operations.ATNC as ATNC,
-		operations.rques as rques
+		operations.rques as rques,
+        patients.nom as pat_lastname, patients.prenom as pat_firstname,
+        patients.incapable_majeur as incapable_majeur, patients.naissance as naissance,
+        patients.ATNC as ATNC, patients.sexe as sexe, patients.adresse as adresse,
+        patients.matricule as matricule, patients.ville as ville, patients.SHS as SHS, patients.cp as cp,
+        patients.tel as tel
 		from operations
 		left join users
 		on users.user_id = operations.chir_id
@@ -38,7 +54,6 @@ $sql = "select operations.operation_id as id, operations.chir_id as chir_id,
 $result = db_loadlist($sql);
 $op = $result[0];
 $op["chir_name"] = "Dr. ".$op["chir_lastname"]." ".$op["chir_firstname"];
-$op["pat_name"] = $op["pat_lastname"]." ".$op["pat_firstname"];
 $op["hour_op"] = substr($op["temp_op"], 0, 2);
 $op["min_op"] = substr($op["temp_op"], 3, 2);
 $op["date_op"] = substr($op["date_op"], 8, 2)."/".substr($op["date_op"], 5, 2)."/".substr($op["date_op"], 0, 4);
