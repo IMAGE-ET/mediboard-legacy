105f2e59f9ae3554f9c6beb7ce1d7079966f7e3a
tomdesp <tomdesp@gmail.com>
2004-11-29 19:45:56 +0000

    Ref: Simplification du 'non-module specific' code Fnc: Début du codage des protocoles

--- index.php
+++ index.php
@@ -7,8 +7,10 @@
 * @author Romain Ollivier
 */
 
-$canRead = !getDenyRead( $m );
-$canEdit = !getDenyEdit( $m );
+// [Begin] non-module specific code
+ 
+$canRead = !getDenyRead($m);
+$canEdit = !getDenyEdit($m);
 
 if (!$canRead) {
 	$AppUI->redirect( "m=public&a=access_denied" );
@@ -16,20 +18,25 @@ if (!$canRead) {
 
 $AppUI->savePlace();
 
-if (isset( $_GET['tab'] )) {
-	$AppUI->setState( 'dPplanningOpIdxTab', $_GET['tab'] );
+if (isset($_GET['tab'])) {
+	$AppUI->setState("{$m}IdxTab", $_GET['tab']);
 }
-$tab = $AppUI->getState( 'dPplanningOpIdxTab' ) !== NULL ? $AppUI->getState( 'dPplanningOpIdxTab' ) : 0;
-$active = intval( !$AppUI->getState( 'dPplanningOpIdxTab' ) );
 
-$titleBlock = new CTitleBlock( 'Planning des chirurgiens', 'dPplanningOp.png', $m, "$m.$a" );
+$tab = $AppUI->getState("{$m}IdxTab");
+$active = intval(!$tab);
+
+// [End] non-module specific code
+
+$titleBlock = new CTitleBlock("Planning des chirurgiens", "$m.png", $m, "$m.$a");
 $titleBlock->addCell();
 $titleBlock->show();
 
-$tabBox = new CTabBox( "?m=dPplanningOp", "{$AppUI->cfg['root_dir']}/modules/dPplanningOp/", $tab );
-$tabBox->add( 'vw_idx_planning', 'Consulter le planning' );
-$tabBox->add( 'vw_edit_planning', 'Modifier une opération' );
-$tabBox->add( 'vw_add_planning', 'Planifier une opération' );
+$tabBox = new CTabBox("?m=$m", "{$AppUI->cfg['root_dir']}/modules/$m/", $tab );
+$tabBox->add("vw_idx_planning", "Consulter le planning");
+$tabBox->add("vw_edit_planning", "Modifier une opération");
+$tabBox->add("vw_add_planning", "Planifier une opération");
+$tabBox->add("vw_protocoles", "Protocoles");
+$tabBox->add("vw_add_protocole", "Ajouter un protocole");
+$tabBox->add("vw_edit_protocole", "Editer un protocole");
 $tabBox->show();
-
 ?>
