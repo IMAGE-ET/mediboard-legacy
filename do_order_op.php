8b1fbbad6338e9619f57102017b0d7ac01f6e4d8
rhum1 <rhum1>
2004-12-02 16:47:12 +0000

    Prise en compte de l'heure de passage dans la gestion de l'ordre des interventions

--- do_order_op.php
+++ do_order_op.php
@@ -20,39 +20,79 @@ $rank = $result[0]["rank"];
 switch($cmd)
 {
   case "insert" : {
-    $sql = "SELECT rank
-			FROM operations
-			WHERE operations.plageop_id = '$plageop'
-			ORDER BY rank DESC";
+    $sql = "SELECT operations.rank AS rank, operations.time_operation AS time,
+            operations.temp_operation as duree, plagesop.debut AS debut
+			FROM operations, plagesop
+			WHERE plagesop.id = '$plageop'
+            AND operations.plageop_id = plagesop.id
+			ORDER BY operations.rank DESC";
 	$result = db_loadlist($sql);
-	$sql = "UPDATE operations
-			SET rank = '".($result[0]["rank"] + 1)."'
-			WHERE operations.operation_id = '$id'";
-	$result = db_exec($sql);
+    if($result[0]["rank"] == 0) {
+      $sql = "UPDATE operations
+              SET operations.time_operation = '".$result[0]["debut"]."',
+              rank = '".($result[0]["rank"] + 1)."'
+			  WHERE operations.operation_id = '$id'";
+      
+    } else {
+      $hour = substr($result[0]["time"], 0, 2) + substr($result[0]["duree"], 0, 2);
+      $min = substr($result[0]["time"], 3, 2) + substr($result[0]["duree"], 3, 2);
+      $time = date("H:i:00", mktime($hour, $min, 0, 1, 1, 2000));
+	  $sql = "UPDATE operations
+              SET operations.time_operation = '$time',
+              rank = '".($result[0]["rank"] + 1)."'
+              WHERE operations.operation_id = '$id'";
+    }
+	$exec = db_exec($sql);
     break;
   }
   case "down" : {
+    $sql = "SELECT time_operation AS time, temp_operation AS duree
+            FROM operations
+            WHERE plageop_id = '$plageop'
+            AND (rank = '$rank' OR rank = '".($rank + 1)."')
+            ORDER by rank";
+	$result = db_loadlist($sql);
+    $hour = substr($result[0]["time"], 0, 2) + substr($result[1]["duree"], 0, 2);
+    $min = substr($result[0]["time"], 3, 2) + substr($result[1]["duree"], 3, 2);
+    $time = date("H:i:00", mktime($hour, $min, 0, 1, 1, 2000));
+    //On fait monter celui qui est en dessous
     $sql = "UPDATE operations
-			SET rank = '$rank'
+			SET rank = '$rank',
+            time_operation = '".$result[0]["time"]."'
 			WHERE operations.plageop_id = '$plageop'
 			AND rank = '".($rank + 1)."'";
-	$result = db_exec($sql);
+	$exec = db_exec($sql);
+    //On fait descendre celui qu'on a choisit
     $sql = "UPDATE operations
-			SET rank = '".($rank + 1)."'
+			SET rank = '".($rank + 1)."',
+            time_operation = '$time'
 			WHERE operations.operation_id = '$id'";
-	$result = db_exec($sql);
+	$exec = db_exec($sql);
     break;
   }
   case "up" : {
+    $sql = "SELECT time_operation AS time, temp_operation AS duree
+            FROM operations
+            WHERE plageop_id = '$plageop'
+            AND (rank = '$rank' OR rank = '".($rank - 1)."')
+            ORDER by rank";
+	$result = db_loadlist($sql);
+    $hour = substr($result[0]["time"], 0, 2) + substr($result[1]["duree"], 0, 2);
+    $min = substr($result[0]["time"], 3, 2) + substr($result[1]["duree"], 3, 2);
+    $time = date("H:i:00", mktime($hour, $min, 0, 1, 1, 2000));
+    //On fait descendre celui qui est au dessus
     $sql = "UPDATE operations
-			SET rank = '$rank'
+			SET rank = '$rank',
+            time_operation = '$time'
 			WHERE operations.plageop_id = '$plageop'
 			AND rank = '".($rank - 1)."'";
-	$result = db_exec($sql);
+	$exec = db_exec($sql);
+    //On fait monter celui qu'on a choisit
     $sql = "UPDATE operations
-			SET rank = '".($rank - 1)."'
+			SET rank = '".($rank - 1)."',
+            time_operation = '".$result[0]["time"]."'
 			WHERE operations.operation_id = '$id'";
-	$result = db_exec($sql);
+	$exec = db_exec($sql);
     break;
   }
   case "sethour" : {
@@ -62,19 +102,37 @@ switch($cmd)
 			SET time_operation = '".$hour.":".$min.":00'
 			WHERE operations.operation_id = '$id'";
 	$result = db_exec($sql);
-    $sql = "SELECT operations.operation_id as id
-            FROM operations
-            WHERE operations.plageop_id = '$plageop'
-            AND operations.rank != '0'
-            ORDER BY operations.time_operation, operations.rank";
-    $result = db_loadlist($sql);
-    $i = 1;
-    foreach($result as $key => $value) {
-      $sql = "UPDATE operations
-              SET rank = '$i'
-              WHERE operations.operation_id = '".$value["id"]."'";
-      db_exec($sql);
-      $i++;
+    $f = 1;
+    while($f) {
+      $f = 0;
+      $sql = "SELECT operations.operation_id AS id,
+              operations.time_operation AS time,
+              operations.temp_operation AS duree
+              FROM operations
+              WHERE operations.plageop_id = '$plageop'
+              AND operations.rank != '0'
+              ORDER BY operations.time_operation, operations.rank";
+      $result = db_loadlist($sql);
+      $i = 1;
+      foreach($result as $key => $value) {    
+        if($key != 0) {
+          $hour = substr($result[$key-1]["time"], 0, 2) + substr($result[$key-1]["duree"], 0, 2);
+          $min = substr($result[$key-1]["time"], 3, 2) + substr($result[$key-1]["duree"], 3, 2);
+          $time = date("H:i:00", mktime($hour, $min, 0, 1, 1, 2000));
+          if($time > $value["time"]) {
+            $sql = "UPDATE operations
+                    SET operations.time_operation = '$time'
+                    WHERE operation_id = '".$value["id"]."'";
+            db_exec($sql);
+            $f = 1;
+          }
+        }
+        $sql = "UPDATE operations
+                SET operations.rank = '$i'
+                WHERE operations.operation_id = '".$value["id"]."'";
+        db_exec($sql);
+        $i++;
+      }
     }
     break;
   }
