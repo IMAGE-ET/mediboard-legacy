8a566c1604581893ed21d5fb91c0dd1ee428d3fd
tomdesp <tomdesp@gmail.com>
2005-01-03 14:09:27 +0000

    Ref: Gestion des permissions

--- prepare_forms.js
+++ prepare_forms.js
@@ -1,26 +1,40 @@
 // $Id$
 
 function prepareForms() {
-  var bGiveFocus = true;
+  var msg = "Rapport: ";
+  
+  var giveFocus = true;
 
+  // Retrieve labels
+  var labels = new Array;
+  var labelIt = 0;
+  while (label = document.getElementsByTagName("label")[labelIt++]) {
+    labels[label.attributes.id] = label;
+    msg += "\nlabel for: " + label.getAttribute('for');
+  }
+  
+  
+  // For each form
   var formIt = 0;
   while (form = document.forms[formIt++]) {
-    var msg = "Formulaire: " + form.name;
-
+    var formName = "Formulaire: " + form.name;
+    
+    // For each element
     var elementIt = 0;
     while (element = form.elements[elementIt++]) {
       // Create id for each element
       element.id = form.name + "_" + element.name;
-      
       if (element.type == "radio") {
         element.id += "_" + element.value;
       }
 
       // Focus on first non hidden input
-      if (bGiveFocus && element.type == "text") {
+      if (giveFocus && element.type == "text") {
         element.focus();
-        bGiveFocus = false;
+        giveFocus = false;
       }
     }
   }
+  
+//  alert (msg);
 }
