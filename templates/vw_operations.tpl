d4133a44182d2dfd8f367a84af4539df6ea48e1e
rhum1 <rhum1>
2005-07-11 03:09:35 +0000

    commit après une nuit blanche de boulot pas de description, désolé (travail sur les modèles, et ajout de modèles d'admission...)

--- templates/vw_operations.tpl
+++ templates/vw_operations.tpl
@@ -9,8 +9,8 @@
       <select name="salle" onchange="this.form.submit()">
         <option value="0">Aucune salle</option>
         {foreach from=$listSalles item=curr_salle}
-        <option value="{$curr_salle.id}" {if $curr_salle.id == $salle} selected="selected" {/if}>
-          {$curr_salle.nom}
+        <option value="{$curr_salle->id}" {if $curr_salle->id == $salle} selected="selected" {/if}>
+          {$curr_salle->nom}
         </option>
         {/foreach}
       </select>
@@ -21,8 +21,8 @@
   {foreach from=$plages item=curr_plage}
   <tr>
     <td>
-      <strong>Dr. {$curr_plage.lastname} {$curr_plage.firstname}
-      de {$curr_plage.debut} à {$curr_plage.fin}</strong>
+      <strong>Dr. {$curr_plage->_ref_chir->_view}
+      de {$curr_plage->debut|date_format:"%Hh%M"} à {$curr_plage->fin|date_format:"%Hh%M"}</strong>
     </td>
   </tr>
   <tr>
@@ -39,35 +39,40 @@
           <th>Entrée en salle</th>
           <th>Sortie de salle</th>
         </tr>
-        {foreach from=$curr_plage.operations item=curr_operation}
+        {foreach from=$curr_plage->_ref_operations item=curr_operation}
         <tr>
-          <td>{$curr_operation.heure}</td>
-          <td class="text">{$curr_operation.CCAM_libelle} (<i>{$curr_operation.CCAM_code}</i>)</td>
-          <td>{$curr_operation.cote}</td>
-          <td>{$curr_operation.type_anesth}</td>
-          <td class="text">{$curr_operation.remarques} {if $curr_operation.mat}({$curr_operation.mat}) {/if}</td>
-          <td>{$curr_operation.nom} {$curr_operation.prenom}</td>
-          <td>{$curr_operation.duree}</td>
+          <td>{$curr_operation->time_operation|date_format:"%Hh%M"}</td>
+          <td class="text">
+            {$curr_operation->_ext_code_ccam->libelleLong} (<i>{$curr_operation->_ext_code_ccam->code}</i>)
+            {if $curr_operation->CCAM_code2}
+            <br />{$curr_operation->_ext_code_ccam2->libelleLong} (<i>{$curr_operation->_ext_code_ccam2->code}</i>)
+            {/if}
+          </td>
+          <td>{$curr_operation->cote}</td>
+          <td>{$curr_operation->_lu_type_anesth}</td>
+          <td class="text">{$curr_operation->rques|nl2br} {if $curr_operation->materiel}({$curr_operation->materiel}) {/if}</td>
+          <td>{$curr_operation->_ref_pat->_view}</td>
+          <td>{$curr_operation->temp_operation|date_format:"%Hh%M"}</td>
           <td align="center">
-            {if $curr_operation.entree}
-            {$curr_operation.entree}
+            {if $curr_operation->entree_bloc}
+            {$curr_operation->entree_bloc}
             {else}
-			<form name="editFrm{$curr_operation.id}" action="index.php" method="get">
+			<form name="editFrm{$curr_operation->operation_id}" action="index.php" method="get">
               <input type="hidden" name="m" value="dPsalleOp" />
               <input type="hidden" name="a" value="do_set_hours" />
-              <input type="hidden" name="entree" value="{$curr_operation.id}" />
+              <input type="hidden" name="entree" value="{$curr_operation->operation_id}" />
               <input type="submit" value="Entrée" />
             </form>
             {/if}
           </td>
           <td align="center">
             {if $curr_operation.sortie}
-            {$curr_operation.sortie}
+            {$curr_operation->sortie_bloc}
             {else}
-            <form name="editFrm{$curr_operation.id}" action="index.php" method="get">
+            <form name="editFrm{$curr_operation->operation_id}" action="index.php" method="get">
               <input type="hidden" name="m" value="dPsalleOp" />
               <input type="hidden" name="a" value="do_set_hours" />
-              <input type="hidden" name="sortie" value="{$curr_operation.id}" />
+              <input type="hidden" name="sortie" value="{$curr_operation->operation_id}" />
               <input type="submit" value="Sortie" />
             </form>
             {/if}
--- vw_operations.php
+++ vw_operations.php
@@ -9,82 +9,41 @@
 
 GLOBAL $AppUI, $canRead, $canEdit, $m;
 
-if (!$canRead) {			// lock out users that do not have at least readPermission on this module
+if (!$canRead) {
 	$AppUI->redirect( "m=public&a=access_denied" );
 }
+
+require_once($AppUI->getModuleClass("mediusers", "functions"));
+require_once($AppUI->getModuleClass("dPbloc", "salle"));
+require_once($AppUI->getModuleClass("dPbloc", "plagesop"));
+require_once($AppUI->getModuleClass("dPplanningOp", "planning"));
 
 $salle = mbGetValueFromGetOrSession("salle", 0);
 $op = mbGetValueFromGetOrSession("op", 0);
 $today = date("Y-m-d");
 
-//Selection des salles
-$sql = "SELECT id, nom
-        FROM sallesbloc";
-$listSalles = db_loadlist($sql);
+// Selection des salles
+$listSalles = new CSalle;
+$listSalles = $listSalles->loadList();
 
-//Selection des plages opératoires de la journée
-$sql = "SELECT users.user_last_name AS lastname,
-        users.user_first_name AS firstname,
-        plagesop.id AS id, plagesop.debut AS debut,
-        plagesop.fin AS fin
-        FROM plagesop
-        LEFT JOIN users
-        ON users.user_username = plagesop.id_chir
-        WHERE date = '$today'
-        AND id_salle = '$salle'
-        ORDER BY plagesop.debut";
-$plages = db_loadlist($sql);
-
-//Operation selectionnée
-$sql = "SELECT *
-        FROM operations
-        WHERE operations.operation_id = '$op'";
-$result = db_loadlist($sql);
-if(count($result) > 0)
-  $selOp = $result[0];
-else
-  $selOp = null;
-
-//Selection des opérations pour chaque plage
-$anesth = dPgetSysVal("AnesthType");
+// Selection des plages opératoires de la journée
+$plages = new CplageOp;
+$where = array();
+$where["date"] = "= '$today'";
+$where["id_salle"] = "= '$salle'";
+$order = "debut";
+$plages = $plages->loadList($where, $order);
 foreach($plages as $key => $value) {
-  $sql = "SELECT operations.operation_id AS id, operations.time_operation AS heure,
-  		  operations.temp_operation AS duree, operations.CCAM_code AS CCAM_code,
-  		  operations.cote as cote, operations.rques AS remarques,
-  		  operations.materiel AS mat, operations.ATNC AS ATNC,
-  		  operations.type_anesth AS code_anesth,
-  		  operations.entree_bloc AS entree, operations.sortie_bloc AS sortie,
-  		  patients.nom AS nom,
-  		  patients.prenom AS prenom
-          FROM operations
-          LEFT JOIN patients
-          ON operations.pat_id = patients.patient_id
-          WHERE operations.plageop_id = '".$value["id"]."'
-          AND operations.rank != 0
-          ORDER BY operations.rank";
-  $plages[$key]["operations"] = db_loadlist($sql);
-}
-$mysql = mysql_connect("localhost", "CCAMAdmin", "AdminCCAM")
-  or die("Could not connect");
-mysql_select_db("ccamV1")
-  or die("Could not select database");
-foreach($plages as $key => $value) {
-  foreach($value["operations"] as $key2 => $value2) {
-  	$plages[$key]["operations"][$key2]["heure"] = substr($value2["heure"], 0, 2)."h".substr($value2["heure"], 3, 2);
-  	$plages[$key]["operations"][$key2]["duree"] = substr($value2["duree"], 0, 2)."h".substr($value2["duree"], 3, 2);
-    if($value2["entree"]) {
-      $plages[$key]["operations"][$key2]["entree"] = substr($value2["entree"], 0, 2)."h".substr($value2["entree"], 3, 2);
-    }
-    if($value2["sortie"]) {
-      $plages[$key]["operations"][$key2]["sortie"] = substr($value2["sortie"], 0, 2)."h".substr($value2["sortie"], 3, 2);
-    }
-    $plages[$key]["operations"][$key2]["type_anesth"] = $anesth[$value2["code_anesth"]];
-    $sql = "select LIBELLELONG from actes where CODE = '".$value2["CCAM_code"]."'";
-    $ccamr = mysql_query($sql);
-    $ccam = mysql_fetch_array($ccamr);
-	$plages[$key]["operations"][$key2]["CCAM_libelle"] = $ccam["LIBELLELONG"];
+  $plages[$key]->loadRefs(0);
+  foreach($plages[$key]->_ref_operations as $key2 => $value) {
+    $plages[$key]->_ref_operations[$key2]->loadRefsFwd();
   }
 }
+
+// Opération selectionnée
+$selOp = new COperation;
+$selOp->load($op);
+$selOp->loadRefsFwd();
 
 // Création du template
 require_once( $AppUI->getSystemClass ('smartydp' ) );
