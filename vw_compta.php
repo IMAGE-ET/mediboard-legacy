3646c9d75dfbe53d27c487466a0a5f162f946661
rhum1 <rhum1>
2005-07-21 06:26:24 +0000

    suppresion de la commande de réparation de la base de données

--- vw_compta.php
+++ vw_compta.php
@@ -13,27 +13,6 @@ require_once( $AppUI->getModuleClass('mediusers', 'functions') );
 require_once( $AppUI->getModuleClass('mediusers') );
 require_once( $AppUI->getModuleClass('dPcabinet', 'tarif') );
 
-// Réparation de la base de données due à des dates de paiement non sauvées
-$sql = "SELECT consultation.consultation_id as consult, plageconsult.date
-        FROM consultation
-        LEFT JOIN plageconsult ON consultation.plageconsult_id = plageconsult.plageconsult_id
-        WHERE date_paiement = '0000-00-00'
-        AND paye =1
-        AND date >= '2005-06-05'
-        AND date <= '2005-07-22'";
-$badRecords = db_loadlist($sql);
-$n = count($badRecords);
-$i = 0;
-foreach($badRecords as $key => $value) {
-  $sql = "UPDATE consultation
-          SET date_paiement = '".$value["date"]."'
-          WHERE consultation_id = '".$value["consult"]."'";
-  if(db_exec($sql))
-    $i++;
-}
-if($n)
-  echo "$n mauvais enregistrements, $i enregistrements corrigés<br />";
-
 $deb = mbDate();
 $fin = mbDate("+ 0 day");
 
