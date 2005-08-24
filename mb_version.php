1a9dbc986a518365ccea0ee8ff7efa1a020bdc76
rhum1 <rhum1>
2005-08-24 07:25:03 +0000

    version 1 : patch de fckeditor

--- mb_version.php
+++ mb_version.php
@@ -4,8 +4,8 @@
 // Manual numbering
 $mb_version_major = 0;
 $mb_version_minor = 3;
-$mb_version_patch = 0;
+$mb_version_patch = 1;
 
 // Automated numbering (should be incremented at each commit)
-$mb_version_build = 1000;
+$mb_version_build = 1;
 ?>
