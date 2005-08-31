98fc12987f36e01793f266d63b3060bfed275ae3
rhum1 <rhum1>
2005-08-31 15:37:57 +0000

    *** empty log message ***

--- send_mail.php
+++ send_mail.php
@@ -10,10 +10,14 @@
 global $AppUI, $canRead, $canEdit, $m;
 
 require_once( $AppUI->getModuleClass('dPcompteRendu', 'templatemanager') );
+//ini_set("include_path", ".;./lib/PEAR");
+//require_once("Mail_IMAPv2/IMAPv2.php");
 
 $templateManager = new CTemplateManager;
 $templateManager->valueMode = false;
 $templateManager->initHTMLArea();
+
+//$getMail = new Mail_IMAPv2();
 
 // Création du template
 require_once( $AppUI->getSystemClass('smartydp'));
