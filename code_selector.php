5fd6e90ea78f581bb8a072dbcf5389c745a6308d
rhum1 <rhum1>
2005-09-26 13:20:08 +0000

    adaptation pour le code CIM10 qui ne marchait plus

--- code_selector.php
+++ code_selector.php
@@ -9,11 +9,12 @@
 
 global $AppUI, $canRead, $canEdit, $m;
 
-if (!$canRead) {			// lock out users that do not have at least readPermission on this module
+if (!$canRead) {
 	$AppUI->redirect( "m=public&a=access_denied" );
 }
 
 require_once( $AppUI->getModuleClass('dPccam', 'acte') );
+require_once( $AppUI->getModuleClass('dPcim10', 'codecim10') );
 
 $list = array();
 $type = dPgetParam( $_GET, 'type', 0 );
@@ -28,53 +29,43 @@ switch($type) {
 				order by favoris_code";
 		$codes = db_loadlist($sql);
 		$i = 0;
-        foreach($codes as $key => $value) {
-          $list[$i] = new CCodeCCAM($value["favoris_code"]);
-          $list[$i]->loadLite();
-          $i++;
-        }
+    foreach($codes as $key => $value) {
+      $list[$i] = new CCodeCCAM($value["favoris_code"]);
+      $list[$i]->loadLite();
+      $i++;
+    }
 		break;
 	}
 	case 'ccam2' : {
 		$sql = "select favoris_code
 				from ccamfavoris
-				where favoris_user = '$chir' or favoris_user = $AppUI->user_id
+				where favoris_user = '$chir' or favoris_user = '$AppUI->user_id'
 				group by favoris_code
 				order by favoris_code";
 		$codes = db_loadlist($sql);
 		$i = 0;
-        foreach($codes as $key => $value) {
-          $list[$i] = new CCodeCCAM($value["favoris_code"]);
-          $list[$i]->loadLite();
-          $i++;
-        }
+    foreach($codes as $key => $value) {
+      $list[$i] = new CCodeCCAM($value["favoris_code"]);
+      $list[$i]->loadLite();
+      $i++;
+    }
 		break;
 	}
 	default : {
-		$sql = "select favoris_code as code
+		$sql = "select favoris_code
 				from cim10favoris
-				where favoris_user = '$chir' or favoris_user = $AppUI->user_id
+				where favoris_user = '$chir' or favoris_user = '$AppUI->user_id'
 				order by favoris_code";
 		$codes = db_loadlist($sql);
-		$mysql = mysql_connect("localhost", "CIM10Admin", "AdminCIM10")
-			or die("Could not connect");
-		mysql_select_db("cim10")
-			or die("Could not select database");
-		$i = 0;
-		foreach($codes as $key => $value)
-		{
-			$query = "select * from master where abbrev = '".$value['code']."'";
-			$result = mysql_query($query);
-			$row = mysql_fetch_array($result);
-			$list[$i]["code"] = $row['abbrev'];
-			$query = "select * from libelle where SID = '".$row['SID']."' and source = 'S'";
-			$result = mysql_query($query);
-			$row = mysql_fetch_array($result);
-			$list[$i]["texte"] = $row['FR_OMS'];
-			$i++;
-		}
-		mysql_close();
-	}
+    $i = 0;
+    foreach($codes as $key => $value) {
+      $list[$i] = new CCodeCIM10($value["favoris_code"]);
+      $list[$i]->loadLite();
+      $list[$i]->libelleLong = $list[$i]->libelle;
+      $i++;
+    }
+    break;
+  }
 }
 
 // Création du template
