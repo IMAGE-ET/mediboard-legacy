6cc1d0afac1497751c8254445a4ab76dbbc8597a
rhum1 <rhum1>
2005-10-04 10:56:49 +0000

    support des logs (encore plein de do_aed a mettre à jour)

--- do_favoris_aed.php
+++ do_favoris_aed.php
@@ -7,45 +7,13 @@
 * @author Romain Ollivier
 */
 
-// create a new instance of the dPccam class
-$obj = new CFavoriCCAM;
-$msg = '';	// reset the message string
+require_once($AppUI->getModuleClass("dPccam", "dPccam"));
+require_once($AppUI->getSystemClass('doobjectaddedit'));
 
-// bind the informations (variables) retrieved via post to the dPccam object
-if (!$obj->bind( $_POST )) {
-	$AppUI->setMsg( $obj->getError(), UI_MSG_ERROR );
-	$AppUI->redirect();
-}
+$do = new CDoObjectAddEdit("CFavoriCCAM", "favoris_id");
+$do->createMsg = "Favori créé";
+$do->modifyMsg = "Favori modifié";
+$do->deleteMsg = "Favori supprimé";
+$do->doIt();
 
-// detect if a delete operation has to be processed
-$del = dPgetParam( $_POST, 'del', 0 );
-
-
-if ($del) {
-	// check if there are dependencies on this object (not relevant for dPccam, left here for show-purposes)
-	if (!$obj->canDelete( $msg )) {
-		$AppUI->setMsg( $msg, UI_MSG_ERROR );
-		$AppUI->redirect();
-	}
-
-	// see how easy it is to run database commands with the object oriented architecture !
-	// simply delete a quote from db and have detailed error or success report
-	if (($msg = $obj->delete())) {
-		$AppUI->setMsg( $msg, UI_MSG_ERROR );			// message with error flag
-		$AppUI->redirect();
-	} else {
-		$AppUI->setMsg( "Favoris supprimé", UI_MSG_ALERT);		// message with success flag
-		$AppUI->redirect( "m=dPccam" );
-	}
-} else {
-	// simply store the added/edited quote in database via the store method of the dPccam child class of the CDpObject provided ba the dPFramework
-	// no sql command is necessary here! :-)
-	if (($msg = $obj->store())) {
-		$AppUI->setMsg( $msg, UI_MSG_ERROR );
-	} else {
-		$isNotNew = @$_POST['favoris_id'];
-		$AppUI->setMsg( $isNotNew ? 'Favoris mis à jour' : 'Favoris inséré', UI_MSG_OK);
-	}
-	$AppUI->redirect();
-}
 ?>
