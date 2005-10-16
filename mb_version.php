754a1bfd52f7851b99f0a91a4c2e7ff4b8f3a35a
rhum1 <rhum1>
2005-10-16 23:40:47 +0000

    version 3.0.3

--- mb_version.php
+++ mb_version.php
@@ -4,8 +4,8 @@
 // Manual numbering
 $mb_version_major = 0;
 $mb_version_minor = 3;
-$mb_version_patch = 2;
+$mb_version_patch = 3;
 
 // Automated numbering (should be incremented at each commit)
-$mb_version_build = 4;
+$mb_version_build = 5;
 ?>
