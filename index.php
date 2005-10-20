1758e1e4dc57be0f574041dc3e06b26af18c833e
rhum1 <rhum1>
2005-10-20 08:55:00 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php /* $Id$ */
+
+/**
+* @package Mediboard
+* @subpackage dPstats
+* @version $Revision$
+* @author Romain Ollivier
+*/
+
+$canRead = !getDenyRead( $m );
+$canEdit = !getDenyEdit( $m );
+
+if (!$canRead) {
+	$AppUI->redirect( "m=public&a=access_denied" );
+}
+
+$AppUI->savePlace();
+
+if (isset( $_GET['tab'] )) {
+	$AppUI->setState( 'dPstatsIdxTab', $_GET['tab'] );
+}
+$tab = $AppUI->getState( 'dPstatsIdxTab' ) !== NULL ? $AppUI->getState( 'dPstatsIdxTab' ) : 0;
+$active = intval( !$AppUI->getState( 'dPstatsIdxTab' ) );
+
+$titleBlock = new CTitleBlock( 'Reporting', 'dPstats.png', $m, "$m.$a" );
+$titleBlock->addCell();
+$titleBlock->show();
+
+$tabBox = new CTabBox( "?m=dPstats", "{$AppUI->cfg['root_dir']}/modules/dPstats/", $tab );
+$tabBox->add( 'vw_activite', 'Activite' );
+$tabBox->show();
+
+?>
---
+++ setup.php
@@ -0,0 +1,60 @@
+<?php /* $Id$ */
+
+/**
+* @package Mediboard
+* @subpackage dPstats
+* @version $Revision$
+* @author Romain Ollivier
+*/
+
+// MODULE CONFIGURATION DEFINITION
+$config = array();
+$config['mod_name'] = 'dPstats';
+$config['mod_version'] = '0.1';
+$config['mod_directory'] = 'dPstats';
+$config['mod_setup_class'] = 'CSetupdPstats';
+$config['mod_type'] = 'user';
+$config['mod_ui_name'] = 'Stats';
+$config['mod_ui_icon'] = 'dPstats.png';
+$config['mod_description'] = 'Reporting';
+$config['mod_config'] = true;
+
+if (@$a == 'setup') {
+	echo dPshowModuleConfig( $config );
+}
+
+class CSetupdPstats {
+
+	function configure() {
+	global $AppUI;
+		$AppUI->redirect( 'm=dPstats&a=configure' );
+  		return true;
+	}
+
+	function remove() {
+
+		return null;
+	}
+
+	function upgrade( $old_version ) {
+		switch ( $old_version )
+		{
+		case "all":
+		case "0.9":
+		case "1.0":
+			return true;
+		default:
+			return false;
+		}
+		return false;
+	}
+
+	function install() {
+		
+		$this->upgrade("all");
+
+		return null;
+	}
+}
+
+?>
