0d6e113467d3e5300bbf699372b28e1ccd6d6df9
rhum1 <rhum1>
2005-11-29 13:40:33 +0000

    déplacement des fichiers .js

---
+++ javascript/gosu/debug.js
@@ -0,0 +1,39 @@
+var debugWindow = null;
+
+function debug(s, name) {
+    if (!debugWindow) {
+        debugWindow = window.open("", "debugWindow", "width=400,height=500,scrollbars=yes,resizable=yes");
+        debugWindow.document.write("<pre>");
+    }
+    
+    if (name) {
+        debugWindow.document.write('<div style="font: 12px sans-serif; font-weight: bold;">'+name+'</div>');
+    }
+    
+    debugWindow.document.write(s + "\n");
+}
+
+function debugObject(obj, name) {
+    var s = typeof obj;
+    for (var i in obj) {
+    	var prop = obj[i];
+    	if (prop) {
+	        if ((typeof prop == "object" || typeof prop == "function") && prop.toString) {
+	            s += "Property." + i + "=" + prop.toString().replace(/\n/g, "") + "\n";
+	        } else {
+	            s += "Property." + i + "=" + prop + "\n";
+	        }
+    	}
+    }
+
+    debug(s, name);
+}
+
+function debugArray(arr, name) {
+    var s = '';
+    for (var i = 0; i < arr.length; ++i) {
+        s += "Array[" + i + "]=" + arr[i] + "\n";
+    }
+    
+    debug(s, name);
+}
