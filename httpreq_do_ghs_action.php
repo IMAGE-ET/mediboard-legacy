cd6be9e61f3465991aae3eb8e971eb9485b6513f
rhum1 <rhum1>
2006-03-27 14:39:03 +0000

    ajout de l'onglet permettant la création des tables pour le groupage en ghm

---
+++ httpreq_do_ghs_action.php
@@ -0,0 +1,545 @@
+<?php /* $Id$ */
+
+/**
+* @package Mediboard
+* @subpackage dPpmsi
+* @version $Revision$
+* @author Romain Ollivier
+*/
+
+global $AppUI, $canRead, $canEdit, $m;
+
+$type = mbGetValueFromGet("type", 0);
+
+//Reconnaissance d'un code Cim10
+$regCim10 = "[[:alpha:]][[:digit:]]{2}[.]?[[:alnum:]\+\*-]*";
+
+// Reconnaissance d'un code CCAM
+$regCCAM = "[[:alpha:]]{4}[[:digit:]]{3}";
+
+switch($type) {
+  case "AddCM" :
+    addcm();
+    break;
+  case "AddDiagCM" :
+    adddiagcm();
+    break;
+  case "AddActes" :
+    addactes();
+    break;
+  case "AddGHM" :
+    addghm();
+    break;
+  case "AddCMA" :
+    addcma();
+    break;
+  case "AddIncomp" :
+    addincomp();
+    break;
+  case "AddArbre" :
+    addarbre();
+    break;
+  default:
+    echo "Argument <strong>type</strong> manquant";
+    break;
+}
+
+/** Ajout des CM, valide pour la version 1010
+ * Fichier texte : ./modules/dPpmsi/ghm/CM.txt
+ * Ligne sous la forme "XX Nom du CM" */
+function addcm() {
+  global $AppUI, $regCim10, $regCCAM;
+  $base = $AppUI->cfg['baseGHS'];
+  $fileName = "./modules/dPpmsi/ghm/CM.txt";
+  do_connect($base);
+
+  // Table des CM
+  $sql = "DROP TABLE IF EXISTS `cm`;";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  }
+  $sql = "CREATE TABLE `cm` (
+  `CM_id` varchar(2) NOT NULL default '0',
+  `nom` varchar(100) default NULL,
+  PRIMARY KEY  (`CM_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des catégoris majeurs';";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  } else {
+    echo "<strong>Done :</strong> Table des CM créée<br />";
+  }
+
+  // Lecture du fichier
+  $file = @fopen($fileName, 'rw');
+  if(!$file) {
+    echo "Fichier non trouvé<br>";
+    return;
+  }
+  
+  $nCM = 0;
+
+  // Ajout des lignes
+  while (!feof($file)) {
+    $id = fgets($file, 3);
+    fgets($file, 2);
+    $nom = fgets($file, 1024);
+    $sql = "INSERT INTO CM values('$id', '".addslashes($nom)."');";
+    db_exec($sql, $base);
+    if($error = db_error($base)) {
+      echo "$error ($sql)<br />";
+    } else {
+      $nCM++;
+      //echo "<strong>Done :</strong> ".$id."-".$nom."<br />";
+    }
+  }
+  echo "<strong>Done :</strong> $nCM CM créés<br />";
+}
+
+/** Ajout des diagnostics d'entrée dans les CM, valide pour la version 1010
+ * Fichier texte : ./modules/dPpmsi/ghm/diagCM.txt */
+function adddiagcm() {
+  global $AppUI, $regCim10, $regCCAM;
+  $base = $AppUI->cfg['baseGHS'];
+  $fileName = "./modules/dPpmsi/ghm/diagCM.txt";
+  do_connect($base);
+  $sql = "DROP TABLE IF EXISTS `diagCM`;";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  }
+  $sql = "CREATE TABLE `diagCM` (
+  `diag` varchar(10) NOT NULL default '0',
+  `CM_id` varchar(2) NOT NULL default '01',
+  PRIMARY KEY  (`diag`, `CM_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des diagnostics d\'entree dans les CM';";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  } else {
+    echo "<strong>Done :</strong> Table des diagnostics d'entrée créée<br />";
+  }
+  $file = @fopen( $fileName, 'rw' );
+  if(! $file) {
+    echo "Fichier non trouvé<br>";
+    return;
+  }
+  $curr_cmd = null;
+  $nCM = 0;
+  $nDiags = 0;
+  while (!feof($file) ) {
+    $line = fgets($file, 1024);
+    if(preg_match("`^Diagnostics d'entrée dans la CMD n° ([[:digit:]]{2})`", $line, $cmd)) {
+      $curr_cmd = $cmd[1];
+      $nCM++;
+    } else if(preg_match("`^($regCim10)`", $line, $diag)) {
+      $sql = "INSERT INTO diagCM VALUES('".$diag[1]."', '$curr_cmd')";db_exec($sql, $base);
+      if($error = db_error($base)) {
+        echo "$error ($sql)<br />";
+      } else {
+        $nDiags++;
+        //echo "<strong>Done :</strong> ".$diag[1]." ($curr_cmd)<br />";
+      }
+    }
+  }
+  echo "<strong>Done :</strong> $nDiags diagnostics créés dans $nCM CM<br />";
+}
+
+/** Ajout des listes d'actes, valide pour la version 1010
+ * Fichier texte : ./modules/dPpmsi/ghm/Actes.txt
+ * Ligne sous la forme
+ * "CMD XX"
+ * "Liste AouD-XXX : nom"
+ * "CCAMXXX/Phase Libelle" */
+function addactes() {
+  global $AppUI, $regCim10, $regCCAM;
+  $base = $AppUI->cfg['baseGHS'];
+  $fileName = "./modules/dPpmsi/ghm/Listes.txt";
+  do_connect($base);
+  $sql = "DROP TABLE IF EXISTS `liste`;";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  }
+  $sql = "CREATE TABLE `liste` (
+  `liste_id` varchar(6) NOT NULL default '0',
+  `nom` varchar(100) default NULL,
+  PRIMARY KEY  (`liste_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des listes';";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  } else {
+    echo "<strong>Done :</strong> Table des listes créée<br />";
+  }
+  $sql = "DROP TABLE IF EXISTS `acte`;";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  }
+  $sql = "CREATE TABLE `acte` (
+  `code` varchar(7) NOT NULL default '0',
+  `phase` varchar(1) NOT NULL default '0',
+  `liste_id` varchar(5) NOT NULL default 'A-001',
+  `CM_id` varchar(2) NOT NULL default '01',
+  PRIMARY KEY  (`code`, `phase`, `liste_id`, `CM_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des actes';";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  } else {
+    echo "<strong>Done :</strong> Table des actes créée<br />";
+  }
+  $sql = "DROP TABLE IF EXISTS `diag`;";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  }
+  $sql = "CREATE TABLE `diag` (
+  `code` varchar(7) NOT NULL default '0',
+  `liste_id` varchar(5) NOT NULL default 'D-001',
+  `CM_id` varchar(2) NOT NULL default '01',
+  PRIMARY KEY  (`code`, `liste_id`, `CM_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des diagnostics';";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  } else {
+    echo "<strong>Done :</strong> Table des diagnostics créée<br />";
+  }
+  $file = @fopen( $fileName, 'rw' );
+  if(! $file) {
+    echo "Fichier non trouvé<br>";
+    return;
+  }
+  $curr_cmd = null;
+  $curr_liste = null;
+  $nCM = 0;
+  $nListes = 0;
+  $nActes = 0;
+  $nDiags = 0;
+  while (!feof($file) ) {
+    $line = fgets($file, 1024);
+    if(preg_match("`^CMD ([[:digit:]]{2})`", $line, $cmd)) {
+      $curr_cmd = $cmd[1];
+      $nCM++;
+    } else if(preg_match("`^Liste ([AD]-[[:digit:]]*) : ([[:alnum:][:space:][:punct:]]*)`", $line, $liste) && $curr_cmd) {
+      $curr_liste = $liste[1];
+      $sql = "INSERT INTO liste VALUES('".$liste[1]."', '".addslashes($liste[2])."')";
+      db_exec($sql, $base);
+      if($error = db_error($base)) {
+        // L'erreur est commentée car certaines listes sont entrées en doublon
+        //echo "$error ($sql)<br />";
+      } else {
+        $nListes++;
+        //echo "<strong>Done :</strong> ".$liste[1]." : ".$liste[2]." (".$curr_cmd.")<br />"; 
+      }
+    } else if(preg_match("`^($regCCAM)/([[:digit:]])`", $line, $acte) && $curr_liste) {
+      $sql = "INSERT INTO acte VALUES('".$acte[1]."', '".$acte[2]."', '$curr_liste', '$curr_cmd')";
+      db_exec($sql, $base);
+      if($error = db_error($base)) {
+        echo "$error ($sql)<br />";
+      } else {
+        $nActes++;
+        //echo "<strong>Done :</strong> ".$acte[1]."/".$acte[2]." ($curr_liste, CMD $curr_cmd)<br />";
+      }
+    } else if(preg_match("`^($regCim10)`", $line, $diag) && $curr_liste) {
+      $sql = "INSERT INTO diag VALUES('".$diag[1]."', '$curr_liste', '$curr_cmd')";
+      db_exec($sql, $base);
+      if($error = db_error($base)) {
+        //echo "$error ($sql)<br />";
+      } else {
+        $nDiags++;
+        //echo "<strong>Done :</strong> ".$diag[1]." ($curr_liste, CMD $curr_cmd)<br />";
+      }
+    }
+  }
+  echo "<strong>Done :</strong> $nCM CM trouvés et $nListes listes, $nActes actes et $nDiags diagnostics créés<br />";
+}
+
+/** Ajout des GHM, valide pour la version 1010
+ * Fichier texte : ./modules/dPpmsi/ghm/GHM.txt */
+function addghm() {
+  global $AppUI, $regCim10, $regCCAM;
+  $base = $AppUI->cfg['baseGHS'];
+  $fileName = "./modules/dPpmsi/ghm/GHM.txt";
+  do_connect($base);
+
+  // Table des CM
+  $sql = "DROP TABLE IF EXISTS `ghm`;";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  }
+  $sql = "CREATE TABLE `ghm` (
+  `GHM_id` varchar(6) NOT NULL default '0',
+  `nom` text default NULL,
+  `groupe` varchar(100) NOT NULL default 'groupes chirurgicaux',
+  `CM_id` varchar(2) NOT NULL default '01',
+  PRIMARY KEY  (`GHM_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des groupements homogènes de malades';";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  } else {
+    echo "<strong>Done :</strong> Table des GHM créée<br />";
+  }
+
+  // Lecture du fichier
+  $file = @fopen($fileName, 'rw');
+  if(!$file) {
+    echo "Fichier non trouvé<br>";
+    return;
+  }
+  
+  $nGHM = 0;
+  $curr_CM = null;
+  $curr_group = null;
+
+  // Ajout des lignes
+  while (!feof($file)) {
+    $line = fgets($file, 1024);
+    if(preg_match("`^CATÉGORIE MAJEURE DE DIAGNOSTIC : ([[:digit:]]{2})`", $line, $cm)) {
+      $curr_CM = $cm[1];
+      //echo "<strong>Done :</strong> Curr_CM = $curr_CM<br />";
+    } else if(preg_match("`^Groupes ([[:alnum:][:space:][:punct:]]*)`", $line, $groupe)) {
+      $curr_group = $groupe[1];
+      //echo "<strong>Done :</strong> Curr_groupe = $curr_groupe<br />";
+    } else if(preg_match("`^([[:digit:]]{2}[[:alpha:]][[:digit:]]{2}[[:alpha:]]) ([[:alnum:][:space:][:punct:]]*)`", $line, $GHM)) {
+      $sql = "INSERT INTO ghm" .
+          "\nvalues('".addslashes($GHM[1])."', '".addslashes($GHM[2])."'," .
+          "\n'".addslashes($curr_group)."', '".addslashes($curr_CM)."');";
+      db_exec($sql, $base);
+      if($error = db_error($base)) {
+        echo "$error ($sql)<br />";
+      } else {
+        //echo "<strong>Done :</strong> ".$id."-".$nom."<br />";
+        $nGHM++;
+      }
+    }
+  }
+  echo "<strong>Done :</strong> $nGHM GHM créés<br />";
+}
+
+/** Ajout des CMA, valide pour la version 1010
+ * Fichiers texte :
+ * ./modules/dPpmsi/ghm/cma.txt
+ * ./modules/dPpmsi/ghm/cmas.txt
+ * ./modules/dPpmsi/ghm/cmasnt.txt */
+function addcma() {
+  global $AppUI, $regCim10, $regCCAM;
+  $base = $AppUI->cfg['baseGHS'];
+  do_connect($base);
+
+  // Table des Complications et Morbidités Associées, CMA Sévères et CMAS Non Traumatiques
+  $listCM = array("cma", "cmas", "cmasnt");
+  foreach($listCM as $typeCM) {
+    //$typeCM = "cma";
+    $sql = "DROP TABLE IF EXISTS `$typeCM`;";
+    db_exec($sql, $base);
+    if($error = db_error($base)) {
+      echo "$error ($sql)<br />";
+    }
+    $sql = "CREATE TABLE `$typeCM` (
+    `".$typeCM."_id` varchar(10) NOT NULL default '0',
+    PRIMARY KEY  (`".$typeCM."_id`)
+  ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des $typeCM';";
+    db_exec($sql, $base);
+    if($error = db_error($base)) {
+      echo "$error ($sql)<br />";
+    } else {
+      echo "<strong>Done :</strong> Table des $typeCM créée<br />";
+    }
+  
+    $fileName = "./modules/dPpmsi/ghm/$typeCM.txt";
+    // Lecture du fichier
+    $file = @fopen($fileName, 'rw');
+    if(!$file) {
+      echo "Fichier non trouvé<br>";
+      return;
+    }
+    
+    $nombre = 0;
+  
+    // Ajout des lignes
+    while (!feof($file)) {
+      $line = fgets($file, 1024);
+      if(preg_match("`^($regCim10)`", $line, $CMA)) {
+        $sql = "INSERT INTO $typeCM values('$CMA[1]');";
+        db_exec($sql, $base);
+        if($error = db_error($base)) {
+          echo "$error ($sql)<br />";
+        } else {
+          $nombre++;
+        }
+      }
+    }
+    echo "<strong>Done :</strong> $nombre $typeCM créés<br />";
+  }
+}
+
+/** Ajout des incompatibilités entre DP - CMA, valide pour la version 1010
+ * Fichier texte : ./modules/dPpmsi/ghm/incomp.txt */
+function addincomp() {
+  global $AppUI, $regCim10, $regCCAM;
+  $base = $AppUI->cfg['baseGHS'];
+  do_connect($base);
+
+  // Table des incompatibilités
+  $sql = "DROP TABLE IF EXISTS `incomp`;";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  }
+  $sql = "CREATE TABLE `incomp` (
+  `CIM1` varchar(10) NOT NULL default '0',
+  `CIM2` varchar(10) NOT NULL default '0',
+  PRIMARY KEY  (`CIM1`, `CIM2`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des incompatibilités DP - CMA';";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  } else {
+    echo "<strong>Done :</strong> Table des incompatibilités créée<br />";
+  }
+
+  $fileName = "./modules/dPpmsi/ghm/incomp.txt";
+  // Lecture du fichier
+  $file = @fopen($fileName, 'rw');
+  if(!$file) {
+    echo "Fichier non trouvé<br>";
+    return;
+  }
+  
+  $nIncomp = 0;
+  $baseCode = null;
+  $n = 0;
+
+  // Ajout des lignes
+  $tabIncomp = array();
+  while (!feof($file)) {
+    $line = fgets($file, 1024);
+    // A t'on au moins un code au début
+    if(preg_match_all("`$regCim10`", $line, $incomp)) {
+      $listIncomp = $incomp[0];
+      // A t'on plus d'un code ?
+      if(count($listIncomp) > 1) {
+        // Sommes nous en début de liste ?
+        if($listIncomp[0] >= $listIncomp[1]) {
+          $baseCode = $listIncomp[0];
+          foreach($listIncomp as $place => $code) {
+            if($place > 0) {
+              $tabIncomp[$baseCode][] = $code;
+            }
+          }
+        } else {
+          foreach($listIncomp as $place => $code) {
+            $tabIncomp[$baseCode][] = $code;
+          }
+        }
+      // A t'on une liste dupliquée ?
+      } else if(preg_match("`même liste que (".$regCim10.")`", $line, $duplicata)){
+        $baseCode = $listIncomp[0];
+        $copy = $duplicata[1];
+        $tabIncomp[$baseCode] = $tabIncomp[$copy];
+      } else {
+        $tabIncomp[$baseCode][] = $listIncomp[0];
+      }
+    }
+    $n++;
+  }
+  //Remplissage de la base
+  foreach($tabIncomp as $baseCode => $liste) {
+    foreach($liste as $code) {
+      $sql = "INSERT INTO incomp VALUES('$baseCode', '$code');";
+      db_exec($sql, $base);
+      if($error = db_error($base)) {
+        echo "$error ($sql)<br />";
+      } else {
+        $nIncomp++;
+      }
+    }
+  }
+  echo "<strong>Done :</strong> $nIncomp incompatibilités créées<br />";
+}
+
+/** Création de l'arbre de décision pour l'orientation vers les GHM
+ * valide pour la version 1010
+ * Fichier CSV : ./modules/dPpmsi/ghm/arbreGHM.csv
+ * première ligne      : nom des colonnes
+ * séparateur          : ',' (virgule)
+ * séparateur du texte : ''' (simple guillemet)
+ */
+
+function addarbre() {
+  global $AppUI, $regCim10, $regCCAM;
+  $base = $AppUI->cfg['baseGHS'];
+  do_connect($base);
+
+  // Table des incompatibilités
+  $sql = "DROP TABLE IF EXISTS `arbre`;";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  }
+
+  $fileName = "./modules/dPpmsi/ghm/arbreGHM.csv";
+  // Lecture du fichier
+  $file = @fopen($fileName, 'rw');
+  if(!$file) {
+    echo "Fichier non trouvé<br>";
+    return;
+  }
+  
+  $line = fgets($file, 1024);
+  $trans = array(
+      "'" => "",
+      "\n" => "",
+      "\r" => "");
+  $line = strtr($line, $trans);
+  $columns = explode(",", $line);
+  
+  $sql = "CREATE TABLE `arbre` (" .
+      "\n`arbre_id` INT(11) NOT NULL auto_increment,";
+  foreach($columns as $column) {
+    $sql .= "\n `$column` VARCHAR(25) DEFAULT NULL,";
+  }
+  $sql .= "\nPRIMARY KEY (`arbre_id`)," .
+      "\nKEY `CM_id` (`CM_id`)" .
+      ") ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table de l\'arbre de décision pour les GHM';";
+  db_exec($sql, $base);
+  if($error = db_error($base)) {
+    echo "$error ($sql)<br />";
+  } else {
+    echo "<strong>Done :</strong> Table de l'arbre de décision créée<br />";
+  }
+  
+  $nPass = 0;
+  $nFailed = 0;
+
+  $trans = array(
+    "\n" => "",
+    "\r" => "",
+    ",," => ",'',");
+  // Ajout des lignes
+  while (!feof($file)) {
+    $line = fgets($file, 1024);
+    $line = strtr($line, $trans);
+    $line = strtr($line, $trans);
+    if(substr($line, -1, 1) == ",")
+      $line .= "''";
+    $sql = "INSERT INTO arbre" .
+        "\nvalues('', $line);";
+    db_exec($sql, $base);
+    if($error = db_error($base)) {
+      echo "$error ($sql)<br />";
+      $nFailed++;
+    } else {
+      //echo "<strong>Done :</strong> $line<br />";
+      $nPass++;
+    }
+  }
+  echo "<strong>Done :</strong> $nPass lignes créés, $nFailed lignes échouées<br />";
+}
+?>
--- templates/datas_ghm.tpl
+++ templates/datas_ghm.tpl
@@ -1,3 +1,16 @@
+{literal}
+<script type="text/javascript">
+
+function doGHSAction(type) {
+  var AddUrl = new Url;
+  AddUrl.setModuleAction("dPpmsi", "httpreq_do_ghs_action");
+  AddUrl.addParam("type", type);
+  AddUrl.requestUpdate(type);
+}
+
+</script>
+{/literal}
+
 <table class="main">
   <tr>
     <td>
@@ -8,14 +21,39 @@
           <th class="category">Résultat</td>
         </tr>
         <tr>
-          <td class="button"><button>Go</button>
-          <td>Création des tables</td>
-          <td id="CreaTab"></td>
+          <td onclick="doGHSAction('AddCM');"><button>Go</button>
+          <td>Remplissage des Catégories Majeures</td>
+          <td class="text" id="AddCM"></td>
         </tr>
         <tr>
-          <td class="button"><button>Go</button>
-          <td>Remplissage des CM</td>
-          <td id="AddCM"></td>
+          <td onclick="doGHSAction('AddDiagCM');"><button>Go</button>
+          <td>Ajout des diagnostics d'entrée dans les CM</td>
+          <td class="text" id="AddDiagCM"></td>
+        </tr>
+        <tr>
+          <td onclick="doGHSAction('AddActes');"><button>Go</button>
+          <td>Ajout des actes/diagnostics dans les listes</td>
+          <td class="text" id="AddActes"></td>
+        </tr>
+        <tr>
+          <td onclick="doGHSAction('AddGHM');"><button>Go</button>
+          <td>Ajout des Groupements Homogènes de Malades</td>
+          <td class="text" id="AddGHM"></td>
+        </tr>
+        <tr>
+          <td onclick="doGHSAction('AddCMA');"><button>Go</button>
+          <td>Ajout des Complications ou Morbidités Associées</td>
+          <td class="text" id="AddCMA"></td>
+        </tr>
+        <tr>
+          <td onclick="doGHSAction('AddIncomp');"><button>Go</button>
+          <td>Ajout des incompatibilités DP - CMA</td>
+          <td class="text" id="AddIncomp"></td>
+        </tr>
+        <tr>
+          <td onclick="doGHSAction('AddArbre');"><button>Go</button>
+          <td>Ajout de l'arbre de décision</td>
+          <td class="text" id="AddArbre"></td>
         </tr>
       </table>
     </td>
