ca2feb12fe0bf1a5c17e82b4a5b0453a8bce8067
rhum1 <rhum1>
2006-04-05 00:02:41 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php /* $Id$ */
+
+/**
+* @package Mediboard
+* @subpackage dPgestionCab
+* @version $Revision$
+* @author Romain Ollivier
+*/
+
+$canRead = !getDenyRead( $m );
+$canEdit = !getDenyEdit( $m );
+
+if (!$canRead) {
+	$AppUI->redirect( "m=public&a=access_denied" );
+}
+
+$AppUI->savePlace();
+
+if (isset( $_GET['tab'] )) {
+	$AppUI->setState( 'dPpmsiIdxTab', $_GET['tab'] );
+}
+$tab = $AppUI->getState( 'dPpmsiIdxTab' ) !== NULL ? $AppUI->getState( 'dPpmsiIdxTab' ) : 0;
+$active = intval( !$AppUI->getState( 'dPpmsiIdxTab' ) );
+
+$titleBlock = new CTitleBlock( 'Gestion comptable de cabinet', 'dPgestionCab.png', $m, "$m.$a" );
+$titleBlock->addCell();
+$titleBlock->show();
+
+$tabBox = new CTabBox( "?m=dPgestionCab", "{$AppUI->cfg['root_dir']}/modules/dPgestionCab/", $tab );
+$tabBox->add( 'edit_compta', 'Comptabilité' );
+$tabBox->add( 'edit_paie', 'Paie' );
+$tabBox->show();
+
+?>
