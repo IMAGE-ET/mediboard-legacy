1f9ae1aa4fda5d7e80dde1dad54f38ac6b111963
tomdesp <tomdesp@gmail.com>
2006-04-21 16:56:38 +0000

    Ref: Déplacement de access_denied

--- code_finder.php
+++ code_finder.php
@@ -12,7 +12,7 @@ global $AppUI, $canRead, $canEdit, $m;
 require_once( $AppUI->getModuleClass('dPcim10', 'codecim10') );
 
 if (!$canEdit) {
-  $AppUI->redirect( "m=public&a=access_denied" );
+  $AppUI->redirect( "m=system&a=access_denied" );
 }
 
 $code = dPgetParam($_GET, "code");
