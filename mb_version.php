be5f62671fb58c01be9fc64f0cc64f3ace2a2dcb
tomdesp <tomdesp@gmail.com>
2006-04-25 14:55:57 +0000

    Erg: Cosmétique sur les configure.php Ref: Authentification des setups

--- mb_version.php
+++ mb_version.php
@@ -3,9 +3,9 @@
 
 // Manual numbering
 $mb_version_major = 0;
-$mb_version_minor = 3;
-$mb_version_patch = 3;
+$mb_version_minor = 4;
+$mb_version_patch = 0;
 
 // Automated numbering (should be incremented at each commit)
-$mb_version_build = 22;
+$mb_version_build = 24;
 ?>
