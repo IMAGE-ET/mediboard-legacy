334ca24c8ea8f8088bc7d3e0fef5247884d96850
rhum1 <rhum1>
2006-04-28 13:27:10 +0000

    temps d'intervention suite et fin

--- templates/vw_time_op.tpl
+++ templates/vw_time_op.tpl
@@ -5,7 +5,7 @@
       <input type="hidden" name="m" value="dPstats" />
       <table class="form">
         <tr>
-          <th colspan="4" class="category">Moyenne des temps opératoires</th>
+          <th colspan="5" class="category">Moyenne des temps opératoires</th>
         </tr>
         <tr>
           <th>Acte CCAM:</th>
@@ -13,10 +13,29 @@
             <input type="text" name="codeCCAM" value="{$codeCCAM}" />
             (% pour tous)
           </td>
+          <th rowspan="2">
+            Intervalle:
+          </th>
+          <td rowspan="2">
+            <input type="radio" name="intervalle" value="0" {if $intervalle == 0}checked="checked"{/if} />
+            Dernier mois
+            <br />
+            <input type="radio" name="intervalle" value="1" {if $intervalle == 1}checked="checked"{/if} />
+            6 dernier mois
+          </td>
+          <td rowspan="2">
+            <input type="radio" name="intervalle" value="2" {if $intervalle == 2}checked="checked"{/if} />
+            Dernière année
+            <br />
+            <input type="radio" name="intervalle" value="3" {if $intervalle == 3}checked="checked"{/if} />
+            Pas d'intervalle
+          </td>
+        </tr>
+        <tr>
           <th>Praticien:</th>
           <td>
             <select name="prat_id">
-              <option value="0">&mdash Tous les praticiens</option>
+              <option value="0">&mdash; Tous les praticiens</option>
               {foreach from=$listPrats item=curr_prat}
               <option value="{$curr_prat->user_id}" {if $curr_prat->user_id == $prat_id}selected="selected"{/if}>
                 {$curr_prat->_view}
@@ -26,7 +45,7 @@
           </td>
         </tr>
         <tr>
-          <td colspan="4" class="button"><button type="submit">Go</button></td>
+          <td colspan="5" class="button"><button type="submit">Go</button></td>
         </tr>
       </table>
       </form>
@@ -35,6 +54,7 @@
           <th>Praticien</th>
           <th>CCAM</th>
           <th>Nombre d'interventions</th>
+          <th>Estimation de durée</th>
           <th>Occupation moyenne de salle</th>
           <th>Durée moyenne d'intervention</th>
         </tr>
@@ -43,6 +63,15 @@
           <td>Dr. {$curr_op.user_last_name} {$curr_op.user_first_name}</td>
           <td>{$curr_op.ccam}</td>
           <td>{$curr_op.total}</td>
+          {if $curr_op.estimation > $curr_op.duree_bloc}
+          <td style="background-color: #aaf;">
+          {elseif $curr_op.estimation < $curr_op.duree_operation}
+          <td style="background-color: #faa;">
+          {else}
+          <td style="background-color: #afa;">
+          {/if}
+            {$curr_op.estimation|date_format:"%Hh%M"}
+          </td>
           <td>
             {$curr_op.duree_bloc|date_format:"%Hh%M"}
             <i>(écart-type : {$curr_op.ecart_bloc|date_format:"%Hh%M"})</i>
@@ -56,13 +85,22 @@
         {if $total.total}
         <tr>
           <th colspan="2">Total</th>
-          <td>{$total.total}</td>
+          <td><strong>{$total.total}</strong></td>
+          {if $total.estimation > $total.duree_bloc}
+          <td style="background-color: #44f;">
+          {elseif $total.estimation < $total.duree_operation}
+          <td style="background-color: #f44;">
+          {else}
+          <td style="background-color: #4f4;">
+          {/if}
+            <strong>{$total.estimation|date_format:"%Hh%M"}</strong>
+          </td>
           <td>
-            {$total.duree_bloc|date_format:"%Hh%M"}
+            <strong>{$total.duree_bloc|date_format:"%Hh%M"}</strong>
             <i>(écart-type : {$total.ecart_bloc|date_format:"%Hh%M"})</i>
           </td>
           <td>
-            {$total.duree_operation|date_format:"%Hh%M"}
+            <strong>{$total.duree_operation|date_format:"%Hh%M"}</strong>
             <i>(écart-type : {$total.ecart_operation|date_format:"%Hh%M"})</i>
           </td>
         </tr>
--- vw_time_op.php
+++ vw_time_op.php
@@ -17,6 +17,7 @@ if (!$canEdit) {
 
 $codeCCAM = strtoupper(mbGetValueFromGetOrSession("codeCCAM", ""));
 $prat_id  = mbGetValueFromGetOrSession("prat_id", 0);
+$intervalle = mbGetValueFromGetOrSession("intervalle", 2);
 
 $user = new CMediusers;
 $listPrats = $user->loadPraticiens(PERM_READ);
@@ -27,7 +28,8 @@ $sql = "SELECT" .
        "\nSEC_TO_TIME(AVG(TIME_TO_SEC(operations.sortie_bloc)-TIME_TO_SEC(operations.entree_bloc))) as duree_bloc," .
        "\nSEC_TO_TIME(STD(TIME_TO_SEC(operations.sortie_bloc)-TIME_TO_SEC(operations.entree_bloc))) as ecart_bloc," .
        "\nSEC_TO_TIME(AVG(TIME_TO_SEC(operations.fin_op)-TIME_TO_SEC(operations.debut_op))) as duree_operation," .
-       "\nSEC_TO_TIME(STD(TIME_TO_SEC(operations.fin_op)-TIME_TO_SEC(operations.debut_op))) as ecart_operation,";
+       "\nSEC_TO_TIME(STD(TIME_TO_SEC(operations.fin_op)-TIME_TO_SEC(operations.debut_op))) as ecart_operation," .
+       "\nSEC_TO_TIME(AVG(TIME_TO_SEC(operations.temp_operation))) AS estimation,";
 if($codeCCAM)
   $sql .= "\n'$codeCCAM' AS ccam";
 else
@@ -35,10 +37,26 @@ else
 $sql .="\nFROM operations" .
        "\nLEFT JOIN users" .
        "\nON operations.chir_id = users.user_id" .
+       "\nLEFT JOIN plagesop" .
+       "\nON operations.plageop_id = plagesop.id" .
        "\nWHERE operations.entree_bloc IS NOT NULL" .
        "\nAND operations.debut_op IS NOT NULL" .
        "\nAND operations.fin_op IS NOT NULL" .
-       "\nAND operations.sortie_bloc IS NOT NULL";
+       "\nAND operations.sortie_bloc IS NOT NULL" .
+       "\nAND operations.entree_bloc < operations.debut_op";
+       "\nAND operations.debut_op < operations.fin_op";
+       "\nAND operations.fin_op < operations.sortie_bloc";
+switch($intervalle) {
+  case 0:
+    $sql .= "\nAND plagesop.date BETWEEN '".mbDate("-1 month")."' AND '".mbDate()."'";
+    break;
+  case 1:
+    $sql .= "\nAND plagesop.date BETWEEN '".mbDate("-6 month")."' AND '".mbDate()."'";
+    break;
+  case 2:
+    $sql .= "\nAND plagesop.date BETWEEN '".mbDate("-1 year")."' AND '".mbDate()."'";
+    break;
+}
 if($prat_id)
   $sql .= "\nAND operations.chir_id = '$prat_id'";
 if($codeCCAM)
@@ -54,12 +72,29 @@ $sql = "SELECT" .
        "\nSEC_TO_TIME(AVG(TIME_TO_SEC(operations.sortie_bloc)-TIME_TO_SEC(operations.entree_bloc))) as duree_bloc," .
        "\nSEC_TO_TIME(STD(TIME_TO_SEC(operations.sortie_bloc)-TIME_TO_SEC(operations.entree_bloc))) as ecart_bloc," .
        "\nSEC_TO_TIME(AVG(TIME_TO_SEC(operations.fin_op)-TIME_TO_SEC(operations.debut_op))) as duree_operation," .
-       "\nSEC_TO_TIME(STD(TIME_TO_SEC(operations.fin_op)-TIME_TO_SEC(operations.debut_op))) as ecart_operation".
+       "\nSEC_TO_TIME(STD(TIME_TO_SEC(operations.fin_op)-TIME_TO_SEC(operations.debut_op))) as ecart_operation," .
+       "\nSEC_TO_TIME(AVG(TIME_TO_SEC(operations.temp_operation))) AS estimation" .
        "\nFROM operations" .
+       "\nLEFT JOIN plagesop" .
+       "\nON operations.plageop_id = plagesop.id" .
        "\nWHERE operations.entree_bloc IS NOT NULL" .
        "\nAND operations.debut_op IS NOT NULL" .
        "\nAND operations.fin_op IS NOT NULL" .
-       "\nAND operations.sortie_bloc IS NOT NULL";
+       "\nAND operations.sortie_bloc IS NOT NULL" .
+       "\nAND operations.entree_bloc < operations.debut_op";
+       "\nAND operations.debut_op < operations.fin_op";
+       "\nAND operations.fin_op < operations.sortie_bloc";
+switch($intervalle) {
+  case 0:
+    $sql .= "\nAND plagesop.date BETWEEN '".mbDate("-1 month")."' AND '".mbDate()."'";
+    break;
+  case 1:
+    $sql .= "\nAND plagesop.date BETWEEN '".mbDate("-6 month")."' AND '".mbDate()."'";
+    break;
+  case 2:
+    $sql .= "\nAND plagesop.date BETWEEN '".mbDate("-1 year")."' AND '".mbDate()."'";
+    break;
+}
 if($prat_id)
   $sql .= "\nAND operations.chir_id = '$prat_id'";
 if($codeCCAM)
@@ -74,6 +109,7 @@ $smarty = new CSmartyDP;
 
 $smarty->assign('prat_id'   , $prat_id);
 $smarty->assign('codeCCAM'  , $codeCCAM);
+$smarty->assign('intervalle'  , $intervalle);
 $smarty->assign('listPrats' , $listPrats);
 $smarty->assign('listOps'   , $listOps);
 $smarty->assign('total'     , $total);
